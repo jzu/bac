dc1e21786594fc8e77af4eca5ce6780d32c0630a
Jean Zundel <jzu@free.fr>
2020-10-15 14:51:26 +0200

    New sigs generator, by scraping the documentation pages

--- gensigs.pl
+++ gensigs.pl
@@ -1,67 +1,79 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
+
+use strict;
+use File::Copy;
 
 # gensigs.pl - Generate API signatures for bac
 # Copyright (C) 2020 Jean Zundel <jzu@free.fr>
 # See https://github.com/jzu/bac
 #
-# Generates Avalanche API signatures as text in bac.sigs.
-# They'll never be accurate I guess but, hey, better than nothing.
+# Generates Avalanche API signatures as text in bac.sigs by
+# scraping https://docs.avax.network/v1.0/en/api/
 # Saves the current bac.sigs in /tmp with a unique suffix.
-# Tempted by Brainfuck? Try Perl first.
+# No EVM stuff yet.
 
-use strict;
-use warnings;
-use Cwd qw (cwd);
-use File::Copy;
+my $docs = "/tmp/sigs.tmp";
+my $sigs = "bac.sigs";
 
-my $cwd = cwd;
-if (chdir "../avalanche-postman-collection") {
-  system ("git pull");
-  chdir $cwd;
+my $scrape = 0;
+my $line = "";
+
+
+sub apis () { qw (admin auth avm health info ipc keystore platform) };
+
+
+-e "/usr/bin/w3m" or
+  die "$0 needs w3m  (apt install w3m) ";
+
+-e $docs and 
+  unlink $docs; 
+copy ("$sigs", "/tmp/$sigs.$$");
+
+foreach my $api (apis()) {
+  print "$api\n";
+  `w3m -dump "https://docs.avax.network/v1.0/en/api/$api"/ >> $docs`
 }
-else {
-  die "No ../avalanche-postman-collection: please git clone " .
-      "https://github.com/ava-labs/avalanche-postman-collection.git " .
-      "in the same parent directory as this one.";
-}
-copy ("bac.sigs", "/tmp/bac.sigs.$$");
 
-open (S, ">bac.sigs");
-open (J, "<../avalanche-postman-collection/Avalanche.postman_collection.json") or die;
+open (I, "<$docs") or die;
+open (O, ">$sigs") or die;
 
-while (<J>) {
-  if (m/raw":.*jsonrpc/) {
-    if (!/avax\./) {
-      s/\\n//g;
-      s/\\//g;
-      s/.*method" *: *"//;
-      s/params//;
-      s/"//g;
-      s/: *\{\{[^\}]*\}\}/:/g;
-      s/\{\{[^\}]*\}\}//g;
-      s/: *[^ ,\[\]]*/:/g;
-      s/, id *: *,//;
-      s/\[[0-9A-Za-z \/,-]*\]/\[\]/g;
-      s/\}\}?, *$//;
-      s/://g;
-      s/, / : /;
-      s/  */ /g;
-      s/ id$//;
-      s/[:,] *$//g;
-      s/(txI?D?) }/$1/;
-      s/\] \} \}/\] /;
-      s/\] \} \} \}/\] /;
-      s/password goes here //g;
-      s/ new timestamp//g;
-      s/\[ /\[/g;
-      s/ \]/\]/g;
-      s/\{ /\{/g;
-      s/ \}/\}/g;
-      s/, *id *$//;
-      s/: *$//;
-      print S;
-    }
+while (<I>) {
+
+  if (m/^Signature/) {
+    $line = "";
+    $scrape = 1;
+  }
+  if ($scrape) {
+    s/.*Signature//;
+    chomp;
+    $line = $line . $_;
+  }
+  if ((m/->/) and ($scrape)) {
+    $line =~ s/: *string//g;
+    $line =~ s/: \[\]string/\[\]/g;
+    $line =~ s/: *int//g;
+    $line =~ s/: *float//g;
+    $line =~ s/: *number//g;
+#$line =~ /admin.alias/ and print "$line\n";
+    $line =~ s/: *JSON//g;
+    $line =~ s/[\(\)]/ /g;
+    $line =~ s/ *\boptional\b/\(opt\)/;
+    $line =~ s/, optional/\(opt\)/g;
+    $line =~ s/, \(opt\)/\(opt\)/g;
+    $line =~ s/{/ /;
+    $line =~ s/},? *{//;
+    $line =~ s/: *{ *optional/\(opt\){/;
+    $line =~ s/: *\[\] *{/\[\]{/;
+    $line =~ s/,//g;
+    $line =~ s/->.*//;
+    $line =~ s/} *$//;
+    $line =~ s/  */ /g;
+    $line =~ s/  *$//g;
+    $line =~ s/ / : /;
+    print O "$line\n";
+    $scrape = 0;
   }
 }
-close S;
-close J;
+close O;
+close I;
+unlink $docs;
